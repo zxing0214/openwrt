ee73dc7e1963229b54c01f4d7b9f8b92897c3525
zxing <1646806+zxing0214@users.noreply.github.com>
2021-11-04 14:06:41 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# update default ip
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
